d662838e9ddab97673c7ca106be069ba6c61f2ec
Adrian Morales <moralesgonzalezadri@gmail.com>
2026-05-21 17:53:11 +0200

    as

--- public/config/cors.php
+++ public/config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => array_filter(explode(',', env('ALLOWED_ORIGINS', 'http://localhost:5173,http://127.0.0.1:5173,https://frontendtfg-phi.vercel.app/'))),
+    'allowed_origins' => [env('https://frontendtfg-phi.vercel.app/', '*')],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
