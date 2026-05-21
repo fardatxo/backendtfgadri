20e566a57702d36611932ece0e7d9da1c75e9994
Adrian Morales <moralesgonzalezadri@gmail.com>
2026-05-21 18:04:31 +0200

    fix: cors config for production

--- public/config/cors.php
+++ public/config/cors.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'paths' => ['api/*'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
     'allowed_origins' => [env('https://frontendtfg-phi.vercel.app/', '*')],
     'allowed_origins_patterns' => [],
