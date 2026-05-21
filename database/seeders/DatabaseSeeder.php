69f934d29962d5dc619f83aa53510f5009bab89b
Adrian Morales <moralesgonzalezadri@gmail.com>
2026-05-21 18:56:30 +0200

    chore: add default admin user creation to database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -8,6 +8,14 @@ class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
+        if (\App\Models\User::count() === 0) {
+            \App\Models\User::create([
+                'name' => 'Administrador',
+                'email' => 'admin@chics.com',
+                'password' => \Illuminate\Support\Facades\Hash::make('admin1234'),
+            ]);
+        }
+
         $this->call([
             ProductSeeder::class,
         ]);
